c116ab66411f3a7d6b0634c02a3f8f6e6cd6c5fa
Enri44 <enry.gerardi@gmail.com>
2023-05-22 20:35:19 +0200

    Aggiunta elementi base per creare un oggetto città

--- src/Citta.java
+++ src/Citta.java
@@ -1,2 +1,59 @@
+import java.util.HashMap;
+
 public class Citta {
+    private int id;
+    private String nome;
+    private Posizione posizione;
+    private double peso;
+    private HashMap<Integer,Double> strade;
+    private Citta citta_precedente;
+
+    public Citta(int id, String nome, Posizione posizione) {
+        this.id = id;
+        this.nome = nome;
+        this.posizione = posizione;
+    }
+
+    /**
+     * Presi l'id di una città e il peso per arrivare a essa dalla città attuale
+     * aggiunge un nuovo elemento alle strade della città
+     * @param id id della città collegata
+     * @param peso peso in carburante per arrivarci dalla città che invoca il metodo
+     */
+    public void aggiungi_strada(int id, double peso){
+        strade.put(id, peso);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Posizione getPosizione() {
+        return posizione;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public HashMap<Integer, Double> getStrade() {
+        return strade;
+    }
+
+    public Citta getCitta_precedente() {
+        return citta_precedente;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setCitta_precedente(Citta citta_precedente) {
+        this.citta_precedente = citta_precedente;
+    }
+
 }
---
+++ src/Posizione.java
@@ -0,0 +1,23 @@
+public class Posizione {
+    private final double x;
+    private final double y;
+    private final double z;
+    public Posizione(double x,double y,double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+}
