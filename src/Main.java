9873c37aca6fa374fdfbdf4656d25dadcaf4cd02
Enri44 <enry.gerardi@gmail.com>
2023-05-23 15:19:18 +0200

    Finito il writer e testato (funziona)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
+import javax.xml.stream.XMLStreamException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws XMLStreamException {
         Reader r = new Reader();
         ArrayList<Citta> mappa = r.leggi_mappa();
         RovineManager m = new RovineManager(mappa);
         ArrayList<Citta> p1 = m.calcola_peso_team1(m.getLista_citta());
         ArrayList<Citta> p2 = m.calcola_peso_team2(m.getLista_citta());
-        for(Citta c : p1)
-            System.out.println(c);
-        for(Citta c : p2)
-            System.out.println(c);
+        Writer w = new Writer();
+        w.scrivi_percorsi(p1, p2);
     }
 }
--- src/Writer.java
+++ src/Writer.java
@@ -1,23 +1,48 @@
 import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class Writer {
-    public void scrivi_percorsi(ArrayList<Citta> percorso1, ArrayList<Citta> percorso2){
+    public void scrivi_percorsi(ArrayList<Citta> percorso1, ArrayList<Citta> percorso2) throws XMLStreamException {
         FileOutputStream risultato;
         XMLOutputFactory output = XMLOutputFactory.newInstance();
-        XMLStreamWriter writer;
+        XMLStreamWriter writer=null;
         {
-            try {//inizializzazione del writer per generare l'xml definitivo
-                risultato = new FileOutputStream("fileXML/percorsiRovine.xml");
-                writer = output.createXMLStreamWriter(risultato);
-                writer.writeStartDocument("UTF-8", "1.0");
-            } catch (Exception e) {
-                System.out.println("Errore nell'inizializzazione del writer:");
-                System.out.println(e.getMessage());
-            }
+        try {//inizializzazione del writer per generare l'xml definitivo
+            risultato = new FileOutputStream("fileXML/percorsiRovine.xml");
+            writer = output.createXMLStreamWriter(risultato);
+            writer.writeStartDocument("UTF-8", "1.0");
+        } catch (Exception e) {
+            System.out.println("Errore nell'inizializzazione del writer:");
+            System.out.println(e.getMessage());
         }
-
+        }
+        writer.writeStartElement("routes");
+        writer.writeStartElement("route");
+        writer.writeAttribute("team", "Tonathiu");
+        writer.writeAttribute("cost", Double.toString(percorso1.get(percorso1.size()-1).getPeso()));
+        writer.writeAttribute("cities", Integer.toString(percorso1.size()));
+        for(Citta c : percorso1){
+            writer.writeEmptyElement("city");
+            writer.writeAttribute("id", Integer.toString(c.getId()));
+            writer.writeAttribute("name", c.getNome());
+        }
+        writer.writeEndElement();
+        writer.writeStartElement("route");
+        writer.writeAttribute("team", "Metztli");
+        writer.writeAttribute("cost", Double.toString(percorso2.get(percorso2.size()-1).getPeso()));
+        writer.writeAttribute("cities", Integer.toString(percorso2.size()));
+        for(Citta c : percorso2){
+            writer.writeEmptyElement("city");
+            writer.writeAttribute("id", Integer.toString(c.getId()));
+            writer.writeAttribute("name", c.getNome());
+        }
+        writer.writeEndElement();
+        writer.writeEndElement();
+        writer.writeEndDocument();
+        writer.flush();
+        writer.close();
     }
 }
